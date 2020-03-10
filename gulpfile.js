db5ff08e7d233361f9b901f9bf474b73a419dcd6
Jorge Medina <jorgemedina@Jorges-MacBook-Pro.local>
2020-03-10 11:27:18 -0400

    Adding Babel Polyfill

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp');
 const minify = require('gulp-minify');
- 
+var babel = require('gulp-babel');
 
 
-gulp.task('default',gulp.series(compress))
+
+gulp.task('default',gulp.series(compile, compress))
 
 
 function compress(){
-    gulp.src(['files/*.js', 'files/*.mjs'])
+    return gulp.src(['compiled/*.js', 'compiled/*.mjs'])
     .pipe(minify())
     .pipe(gulp.dest('build'))
+}
+
+function compile(){
+    return gulp.src(['files/*.js', 'files/*.mjs'])
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest('compiled'))
 }
